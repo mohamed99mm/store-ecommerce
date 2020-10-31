95c5aa9441ea7f30f1f0d1ba0507dfb2c85de794
Mohamed Aboelnaga <mohamedaboelnaga37@gmail.com>
2020-10-31 05:55:09 +0200

    =end shippings

--- app/Http/Controllers/Dashboard/SettingsController.php
+++ app/Http/Controllers/Dashboard/SettingsController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 Use App\Models\Setting;
-
+use App\Http\Requests\ShippingRequest;
+use DB;
 class SettingsController extends Controller
 {
 
@@ -18,7 +19,7 @@ class SettingsController extends Controller
 
 
         elseif ($type === 'inner')
-            $shippingMethod = Setting::where('key', 'local_label')->first();
+             $shippingMethod = Setting::where('key', 'local_label')->first();
 
         elseif ($type === 'outer')
             $shippingMethod = Setting::where('key', 'outer_label')->first();
@@ -30,12 +31,26 @@ class SettingsController extends Controller
 
     }
 
-    public function updateShippingMethod(Request $request,$id)
+    public function updateShippingMethod(ShippingRequest $request,$id)
     {
+      try {
+          //validation
 
-        //validation
 
-        //update database
+          DB::beginTransaction();
+          //update database
+          $shippingMethod = Setting::find($id);
+          $shippingMethod->update(['plain_value' => $request->plain_value]);
 
+          //save translation
+          DB::commit();
+          $shippingMethod->value = $request->value;
+          $shippingMethod->save();
+          return redirect() -> back() ->with(['success' => 'تم التحديث بنجاح']);
+      }catch (\Exception $ex)
+      {
+          return redirect() -> back() ->with(['error' => 'هناك خطأ ما يرجى المحاوله فيما بعد ']);
+        DB::rollback();
+      }
     }
 }
--- app/Http/Requests/ShippingRequest.php
+++ app/Http/Requests/ShippingRequest.php
@@ -13,7 +13,7 @@ class ShippingRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,21 @@ class ShippingRequest extends FormRequest
      */
     public function rules()
     {
+        // exists:settings means that the id must exist in table settings
+        //'plain_value'=> 'required|nullable|numeric' means that th e value of plain_value could be null
+        // and if i enter it must be a number
         return [
-            //
+            'id' => 'required|exists:settings',
+            'value'=> 'required',
+            'plain_value'=> 'nullable|numeric',
         ];
+    }
+    public  function messages()
+    {
+        return [
+            'value.required'=> 'يجب ادخال نوع وسيله التوصيل  ',
+
+        ];
+
     }
 }
